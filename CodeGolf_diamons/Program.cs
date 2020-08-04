8ec50be49d63af4c37cdabc442ffdafbf0782e60
pete <petejunk@bluesmith.com>
2020-08-03 21:55:57 -0500

    Cleaned up all compiler warning in BrainSqrt Copied it off to BruteForce.cpp Updated .gitignore ti include solutions Added CodeGolf_diamons (sic) to repo

---
+++ CodeGolf_diamons/Program.cs
@@ -0,0 +1,57 @@
+﻿// 204
+//using System;class x{static void Main(){int t=0,x,y,n;for(;t++<9;){for(y=0;++y<t*2;){for(x=-10;++x<10;){n=t-Math.Abs(y-t)-(x<0?-x:x);Console.Write((n>0?""+n:" ")+(x==9?("\n"+(y==t*2-1?"\n":"")):""));}}}}}
+
+/* 204
+using System;class x{static void Main(){
+int t=0,x,y,n;
+for(;t++<9;){
+for(y=0;++y<t*2;){
+for(x=-10;++x<10;){
+n=t-Math.Abs(y-t)-(x<0?-x:x);
+Console.Write((n>0?""+n:" ")+(x==9?("\n"+(y==t*2-1?"\n":"")):""));
+}}}}}
+*/
+
+#if FALSE
+using System;class x{static void Main(){
+int t=0,x,y,n;
+for(;t++<9;){
+for(y=0;++y<t*2;){
+for(x=-10;++x<10;){
+n=t-Math.Abs(y-t)-(x<0?-x:x);
+Console.Write((n>0?""+n:" ")+(x==9?("\n"+(y==t*2-1?"\n":"")):""));
+}}}}}
+#endif
+
+
+using System;
+class x
+{
+    static void S(int s)
+    {
+        while (s-- > 0)
+        {
+            Console.Write(" ");
+        }
+    }
+    static void L(int n)
+    {
+        for (int a = 1; a <= n*2-1; a++)
+        {
+            Console.Write(n-Math.Abs(a-n));
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        S(9);
+        L(1);
+        L(0);
+
+        S(9); L(1);
+        S(8); L(2);
+        S(9); L(1);
+        L(0);
+    }
+}
