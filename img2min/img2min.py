6e61d6097d96374ba477e51f8650f79883a53e94
Peter Carter <petejunk@bluesmith.com>
2021-01-08 13:43:11 -0600

    Switched random algorithm out for an iterative approach. Needs optimizations, but initial tests are promising

--- img2min/img2min.py
+++ img2min/img2min.py
@@ -1,6 +1,5 @@
 import math
 import heapq
-import random
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageChops
@@ -71,7 +70,7 @@ def compute_delta_from_value(img, rect, rgb):
 # - In-progress image
 # Outputs
 # - Next rectangle to draw (improvment, rect, rgb)
-def get_next_best_rect(ground_img, progress_img, max_rect_size, iterations):
+def get_next_best_rect(ground_img, progress_img, rect_size, iterations):
     # Compute difference between images
     delta_img = ImageChops.difference(ground_img, progress_img)
     h = ground_img.height
@@ -79,27 +78,25 @@ def get_next_best_rect(ground_img, progress_img, max_rect_size, iterations):
 
     # Evaluate different rects to see where the most improvement can happen
     best = (0, (0, 0, 0, 0), (0, 0, 0))
-    for x in range(0, iterations):
+    iters = max(iterations, 1)
+    x_slices = math.ceil(iters * w / rect_size[0])
+    y_slices = math.ceil(iters * h / rect_size[1])
+    for y in range(0, y_slices):
+        for x in range(0, x_slices):
+            x1 = min(x * rect_size[0] / iters, w - rect_size[0] - 1)
+            y1 = min(y * rect_size[1] / iters, h - rect_size[1] - 1)
+            x2 = x1 + rect_size[0]
+            y2 = y1 + rect_size[1]
+            
+            rect = (int(x1), int(y1), int(x2), int(y2))
 
-        x1 = random.randrange(w)
-        y1 = random.randrange(h)
-        x2 = x1
-        y2 = y1
-        while x2 == x1 or y2 == y1:
-            x2 = random.randrange(w)
-            y2 = random.randrange(h)
-        rect = (min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2))
-        #x1 = random.randrange(w - max_rect_size[0])
-        #y1 = random.randrange(h - max_rect_size[1])
-        #rect = (int(x1), int(y1), int(x1 + max_rect_size[0]), int(y1 + max_rect_size[1]))
-
-        avg_rgb = to_int_rgb(compute_average(ground_img, rect))
-        current_delta = compute_sum(delta_img, rect)
-        delta_from_avg = compute_delta_from_value(ground_img, rect, avg_rgb)
-        improvement = current_delta - delta_from_avg
-        if (improvement > best[0]):
-            best = (improvement, rect, avg_rgb)
-            #print(best)
+            avg_rgb = to_int_rgb(compute_average(ground_img, rect))
+            current_delta = compute_sum(delta_img, rect)
+            delta_from_avg = compute_delta_from_value(ground_img, rect, avg_rgb)
+            improvement = current_delta - delta_from_avg
+            if (improvement > best[0]):
+                best = (improvement, rect, avg_rgb)
+                # print(best)
 
     return best
 
@@ -117,49 +114,50 @@ def build_rectangle_list_from_image(img, max_num_rects):
     out_draw = ImageDraw.Draw(out_img)
 
     # How many times the image's size should the iterations cover?
-    ITERATION_SCALAR = 1.0
+    ITERATION_SCALAR = 4.0
 
     while (len(out_rects) < max_num_rects):
         percent_done = len(out_rects) / max_num_rects
-        t = math.pow(percent_done, 2)
-        denom = 2.0 * (1 - t) + img.width * (t)
+        t = math.pow(percent_done, 2.0)
+        denom = 2.0 * (1 - t) + (img.width / 1.5) * (t)
         s = int(img.width / denom)
         size = (s, s)
 
-        img_pixels = img.width * img.height
-        rect_pixels = size[0] * size[1]
-        #iterations = int(max(1, ITERATION_SCALAR * img_pixels / rect_pixels))
-        iterations = 1000
+        next_rect = get_next_best_rect(img, out_img, size, ITERATION_SCALAR)
 
-        next_rect = get_next_best_rect(img, out_img, size, iterations)
         out_rects.append(next_rect)
         out_draw.rectangle(xy=next_rect[1], fill=next_rect[2])
-        print(next_rect)
+        print(s, next_rect)
     
     out_img.save('out_img.png')
 
     return out_rects
 
-def print_mindustry_commands(rect_list, img):
+def print_mindustry_commands(rect_list, img, flush_frequency = 0):
     height = img.height
 
     print("Mindustry Commands ---------------------------------------------------------")
+    print("jump 2 equal done false")
+    print("end")
     print("draw clear 0 0 0 0 0 0")
-    for r in rect_list:
+    for i, r in enumerate(rect_list):
         print("draw color {0} {1} {2} 255 0 0".format(r[2][0], r[2][1], r[2][2]))
         x = r[1][0]
         y = (height - 1) - r[1][3]
-        w = abs(r[1][2] - x)
-        h = abs(r[1][1] - r[1][3])
+        # Add 1 to width and height because Mindustry addressing things in slightly different way than pillow
+        w = abs(r[1][2] - x) + 1
+        h = abs(r[1][1] - r[1][3]) + 1
         print("draw rect {0} {1} {2} {3} 0 0".format(x, y, w, h))
+        if (flush_frequency > 0) and ((i + 1) % flush_frequency == 0):
+            print("drawflush display1")
     print("drawflush display1")
+    print("set done true")
     print("end Mindustry Commands -----------------------------------------------------")
 
 def main():
     dimensions = (80, 80)
-    max_rects = 100
-    random.seed(10)
-
+    max_rects = 300
+    
     #read the image
     image = Image.open("philip_512.jpg")
     image.convert("RGB")
@@ -169,7 +167,7 @@ def main():
 
     print(rect_list)
 
-    print_mindustry_commands(rect_list, base_img)
+    print_mindustry_commands(rect_list, base_img, 10)
 
 if __name__ == "__main__":
 	main()
