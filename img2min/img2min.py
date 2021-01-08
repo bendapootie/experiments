fe4b97e768969bdb0dcf9e59d11847893aaba911
Peter Carter <petejunk@bluesmith.com>
2021-01-08 15:16:42 -0600

    Dramatically improved performance by eliminating duplicate work In a single pass, multiple "best" rects of the same size are found. Potential rects that overlap are discarded

--- img2min/img2min.py
+++ img2min/img2min.py
@@ -65,19 +65,43 @@ def compute_delta_from_value(img, rect, rgb):
 
     return rgb_sum[0] + rgb_sum[1] + rgb_sum[2]
 
+
+# Input
+# - two rects (x1, y1, x2, y2)
+def rects_overlap(r1, r2):
+    # If one rectangle is on left side of other 
+    if(r1[2] <= r2[0] or r2[2] <= r1[0]):
+        return False
+  
+    # If one rectangle is above other
+    if(r1[3] <= r2[1] or r2[3] <= r1[1]):
+        return False
+
+    return True
+
+# Input
+# - test_rect (improvement, (rect), (rgb))
+# - List of (improvement, (rect), (rgb)) to compare to
+def test_rect_options_for_overlap(test_rect, rect_list):
+    for r in rect_list:
+        if rects_overlap(test_rect[1], r[1]):
+            return True
+    return False
+
 # Inputs
 # - Ground truth image
 # - In-progress image
 # Outputs
 # - Next rectangle to draw (improvment, rect, rgb)
-def get_next_best_rect(ground_img, progress_img, rect_size, iterations):
+def get_next_best_rects(ground_img, progress_img, rect_size, iterations, max_rects_to_return):
     # Compute difference between images
     delta_img = ImageChops.difference(ground_img, progress_img)
     h = ground_img.height
     w = ground_img.width
 
     # Evaluate different rects to see where the most improvement can happen
-    best = (0, (0, 0, 0, 0), (0, 0, 0))
+    # List of (improvement, (rect), (rgb))
+    potential_options = []
     iters = max(iterations, 1)
     x_slices = math.ceil(iters * w / rect_size[0])
     y_slices = math.ceil(iters * h / rect_size[1])
@@ -94,11 +118,16 @@ def get_next_best_rect(ground_img, progress_img, rect_size, iterations):
             current_delta = compute_sum(delta_img, rect)
             delta_from_avg = compute_delta_from_value(ground_img, rect, avg_rgb)
             improvement = current_delta - delta_from_avg
-            if (improvement > best[0]):
-                best = (improvement, rect, avg_rgb)
-                # print(best)
-
-    return best
+            potential_options.append((improvement, rect, avg_rgb))
+    
+    best_options = []
+    potential_options.sort()
+    while len(potential_options) > 0 and len(best_options) < max_rects_to_return:
+        rect = potential_options.pop()
+        if (test_rect_options_for_overlap(rect, best_options) == False):
+            best_options.append(rect)
+    
+    return best_options
 
 # Inputs
 # - Ground truth image
@@ -119,15 +148,18 @@ def build_rectangle_list_from_image(img, max_num_rects):
     while (len(out_rects) < max_num_rects):
         percent_done = len(out_rects) / max_num_rects
         t = math.pow(percent_done, 2.0)
-        denom = 2.0 * (1 - t) + (img.width / 1.5) * (t)
+        denom = 3.0 * (1 - t) + (img.width / 1.5) * (t)
         s = int(img.width / denom)
         size = (s, s)
 
-        next_rect = get_next_best_rect(img, out_img, size, ITERATION_SCALAR)
+        max_rects_to_return = min(10, max_num_rects - len(out_rects))
+        next_rects = get_next_best_rects(img, out_img, size, ITERATION_SCALAR, max_rects_to_return)
 
-        out_rects.append(next_rect)
-        out_draw.rectangle(xy=next_rect[1], fill=next_rect[2])
-        print(s, next_rect)
+        #out_rects.append(next_rect)
+        out_rects.extend(next_rects)
+        for r in next_rects:
+            out_draw.rectangle(xy=r[1], fill=r[2])
+            print(s, r)
     
     out_img.save('out_img.png')
 
@@ -156,7 +188,7 @@ def print_mindustry_commands(rect_list, img, flush_frequency = 0):
 
 def main():
     dimensions = (80, 80)
-    max_rects = 300
+    max_rects = 470
     
     #read the image
     image = Image.open("philip_512.jpg")
